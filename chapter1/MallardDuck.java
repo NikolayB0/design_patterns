0ef6111e6982da29f89e9ac21b5a179d6aef2626
Niks132 <nickob132@gmail.com>
2022-01-29 20:43:20 +0000

    Initial Duck code

---
+++ chapter1/MallardDuck.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class MallardDuck extends Duck{
+    
+    public MallardDuck() {
+        quackBehavoir = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
+    public void display() {
+        System.out.println("I'm a real Mallard Duck");
+    }
+}
